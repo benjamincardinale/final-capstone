aa2d6911474f530c8045edac53b4f1973ef61133
Terrell Mallory <Terrell.Mallory@gmail.com>
2021-08-02 12:24:01 -0400

    "pushing to Git"

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,9 +1,27 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
-      <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
-    </div>
-    <router-view />
+    <header>
+      <div id="nav">
+        <router-link v-bind:to="{ name: 'home' }">Home</router-link
+        >&nbsp;|&nbsp;
+        <router-link
+          v-bind:to="{ name: 'logout' }"
+          v-if="$store.state.token != ''"
+          >Logout</router-link
+        >
+      </div>
+      <router-view />
+    </header>
+
+    <aside class="sidebar-left">
+    
+    </aside>
+    <p>
+
+    </p>
+    <footer>
+
+    </footer>
+    
   </div>
 </template>
