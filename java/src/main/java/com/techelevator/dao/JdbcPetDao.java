60e99535325f3ed5ecd46015b2db9d26a71d4505
Henry <hencutJohnson@gmail.com>
2021-08-02 11:42:06 -0400

    Added a mapRowToPet method and a simple method with some placeholder names to retrieve a list of all pets from some SQL db

--- java/src/main/java/com/techelevator/dao/JdbcPetDao.java
+++ java/src/main/java/com/techelevator/dao/JdbcPetDao.java
@@ -2,9 +2,13 @@ package com.techelevator.dao;
 
 import com.techelevator.model.Pet;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class JdbcPetDao implements PetDao{
 
         private JdbcTemplate jdbcTemplate;
@@ -15,6 +19,24 @@ public class JdbcPetDao implements PetDao{
 
     @Override
     public List<Pet> getAllPets() {
-        return null;
+        List<Pet> allPets = new ArrayList<>();
+        String sql = "SELECT * from _pet_;";
+        SqlRowSet result = this.jdbcTemplate.queryForRowSet(sql);
+        while (result.next()){
+            allPets.add(mapRowToPet(result));
+        }
+        return allPets;
+    }
+
+    private Pet mapRowToPet(SqlRowSet row) {
+            Pet newPet = new Pet();
+            newPet.setId(row.getLong("pet_id"));
+            newPet.setName(row.getString("pet_name"));
+            newPet.setNeutered(row.getBoolean("neutered"));
+            newPet.setGender(row.getString("gender"));
+            newPet.setSpecies(row.getString("species"));
+            newPet.setDescription(row.getString("description"));
+            newPet.setAge(row.getInt("age"));
+            return newPet;
     }
 }
