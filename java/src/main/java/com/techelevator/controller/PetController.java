78939d1a6c02a8d5733e7e6b5d0edc53f1a71080
Henry <hencutJohnson@gmail.com>
2021-08-02 11:47:38 -0400

    Added a PetController with a single method in it to retrieve a list of all pets from the endpoint "/"
    No tests have been run, but theoretically now, the backend for the first story
    should be pretty much complete.

---
+++ java/src/main/java/com/techelevator/controller/PetController.java
@@ -0,0 +1,25 @@
+package com.techelevator.controller;
+
+import com.techelevator.dao.JdbcPetDao;
+import com.techelevator.model.Pet;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin
+public class PetController {
+    private JdbcPetDao jdbcPetDao;
+
+    public PetController(JdbcPetDao jdbcPetDao) {
+        this.jdbcPetDao = jdbcPetDao;
+    }
+
+    @RequestMapping(path="/", method = RequestMethod.GET)
+    public List<Pet> getAllPets(){
+        return this.jdbcPetDao.getAllPets();
+    }
+}
