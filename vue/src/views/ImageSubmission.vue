3678100fbe9c0dbfe633124ce05cc5d3e2cc5642
Nate Shrader <84872646+shradez@users.noreply.github.com>
2021-08-05 14:20:17 -0400

    Current progress on submitting images

---
+++ vue/src/views/ImageSubmission.vue
@@ -0,0 +1,17 @@
+<template>
+  <image-submission-form />
+</template>
+
+<script>
+import ImageSubmissionForm from '../components/ImageSubmissionForm'
+
+export default {
+    components: { ImageSubmissionForm },
+    name: "image-submission-view"
+
+}
+</script>
+
+<style>
+
+</style>
