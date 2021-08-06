4020044cddab7c92e888c3329642b1ff795f4347
Terrell Mallory <Terrell.Mallory@gmail.com>
2021-08-06 09:26:49 -0400

    warmer color palette

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,10 +1,7 @@
 <template>
   <div id="app">
-    <div id="nav">
-    
-    </div>
-    <the-header />
     <nav-panel />
+    <the-header />
     <hidden-functions/>
     <router-view class="routerView" />
     <the-footer />
@@ -29,10 +26,34 @@ export default {
 
 <style>
 body {
-  background-image: url("assets/background_image.jpg");
+  /*background-image: url("assets/background_image.jpg");*/
+  background: rgb(234, 231, 220);
   background-attachment: fixed;
+ /*display:grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-template-areas:
+   "nav-panel the-header the-header the-header"
+   "nav-panel hidden-functions router-view router-view"
+   "nav-panel router-view router-view router-view "
+   "nav-panel the-footer the-footer the-footer";*/
 }
 .routerView{
   text-align: center;
+}
+
+the-header {
+  grid-area: the-header;
+}
+nav-panel{
+  grid-area: nav-panel;
+}
+hidden-functions{
+  grid-area: hidden-functions;
+}
+router-view{
+  grid-area: router-view;
+}
+footer{
+  grid-area: footer;
 }
 </style>
--- vue/src/components/TheHeader.vue
+++ vue/src/components/TheHeader.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="the-header">
-    <img class="header-logo" src="../assets/Shelter-Logo.jpg" alt="" />
+    <img class="header-logo" src="../assets/Shelter_Logo_Trans.png" alt="" />
     <header class="header-text">
       <h1>Second Chances</h1>
       <h2>Pet Placement Agency</h2>
@@ -18,36 +18,34 @@ export default {
 .the-header {
   display: flex;
   flex-direction: row;
-  background-color: rgba(241, 138, 41, 0.219);
+  background: linear-gradient(90deg, rgba(233, 128, 116), rgba(233, 128, 116),rgba(255,203,154,.5)/*rgba(214, 126, 126, 0.479)*/);
   border-radius: 25px;
   padding-top: 15px;
   font-family: "Gill Sans", "Gill Sans MT", Calibri, "Trebuchet MS", sans-serif;
-  margin-bottom: 1px;
+  margin-bottom: 100px;
   text-align: center;
   justify-content: space-around;
-
+  margin-top: 50px;
 }
 
 .header-text h1 {
   margin-top: -0.3em;
   margin-bottom: -0.4em;
   text-decoration: underline;
-  font-size: 8em;
+  font-size: 100px;
 }
 h2 {
-  font-size: 3em;
+  font-size: 45px;
   margin-bottom: 10px;
 }
-.header-logo{
-    max-height: 150px;
-    max-width: 140px;
-    border-radius: 25px;
-    opacity: .5;
-    flex-grow:1;
-   margin-left: 200px;
+.header-logo {
+  max-height: 150px;
+  max-width: 140px;
+  flex-grow: 1;
+  margin-left: 300px;
 }
-.header-text{
-    flex-grow:2;
-    padding-right: 200px;
+.header-text {
+  flex-grow: 2;
+  padding-right: 350px;
 }
 </style>
