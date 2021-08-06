781b21e50d5baa74159a25583556ff42ffe34c8d
Benjamin <bcardinale.0@gmail.com>
2021-08-06 16:05:47 -0400

    further work on component for vol approv and view/route

--- vue/src/components/VolunteerApproval.vue
+++ vue/src/components/VolunteerApproval.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="main">
+      <h2 class= "title"> Volunteers Awaiting Approval</h2>
       <table class="volunteer-table">
           <tr class="table-head">
+              <th>ID</th>
               <th>Username</th>
               <th>First name</th>
               <th>Last name</th>
@@ -9,7 +11,16 @@
               <th>Adopted before</th>
               <th>Certifcations</th>
           </tr>
-          
+            <tr class="volunteer-object-loop">
+                <td>{{item.id}}</td>
+                <td>{{item.userName}}</td>
+                <td>{{item.firstName}}</td>
+                <td>{{item.lastName}}</td>
+                <td>{{item.emailAddress}}</td>
+                <td>{{item.adoptedBefore}}</td>
+                <td>{{item.hasCertifications}}</td>
+
+            </tr>
 
       </table>
 
@@ -24,6 +35,16 @@ export default {
 
     data() {
         return {
+            approvedVolunteer: {
+                id: "",
+                userName: "",
+                firstName: "",
+                lastName: "",
+                emailAddress: "",
+                adoptedBefore: "",
+                hasCertification: "",
+                status: "", //use "approved" "declined"
+            }
 
         }
     }
