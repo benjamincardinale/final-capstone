401eea51294b262ea6bdda42f11237b7182c91f3
Benjamin <bcardinale.0@gmail.com>
2021-08-06 15:40:30 -0400

    Active status added to nav panel starting on admin page for vol approval

---
+++ vue/src/components/VolunteerApproval.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="main">
+      <table class="volunteer-table">
+          <tr class="table-head">
+              <th>Username</th>
+              <th>First name</th>
+              <th>Last name</th>
+              <th>E-mail</th>
+              <th>Adopted before</th>
+              <th>Certifcations</th>
+          </tr>
+          
+
+      </table>
+
+
+
+  </div>
+</template>
+
+<script>
+export default {
+    name: "volunteer-approval",
+
+    data() {
+        return {
+
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
