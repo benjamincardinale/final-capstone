24335f06bf5e9d9076d286a5cac4dd3e4766c295
Nate Shrader <84872646+shradez@users.noreply.github.com>
2021-08-07 17:02:25 -0400

    Trying again to be able to post images to imgbb. Unsure how to set up a REST Controller to use as a pass through.

---
+++ java/src/main/java/com/techelevator/controller/ImageController.java
@@ -0,0 +1,43 @@
+package com.techelevator.controller;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@CrossOrigin
+@PreAuthorize("isAuthenticated()")
+public class ImageController {
+
+    @RequestMapping(value = "/upload", method = RequestMethod.GET)
+    public @ResponseBody
+    String provideUploadInfo() {
+        return "You can upload a file by posting to this same URL.";
+    }
+
+    @RequestMapping(value = "/upload", method = RequestMethod.POST)
+    public @ResponseBody
+    String handleFileUpload(@RequestParam("name") String name,
+                            @RequestParam("file") MultipartFile file) {
+        if (!file.isEmpty()) {
+            try {
+                byte[] bytes = file.getBytes();
+                BufferedOutputStream stream =
+                        new BufferedOutputStream(new FileOutputStream(new File(name)));
+                stream.write(bytes);
+                stream.close();
+                return "You successfully uploaded " + name + "!";
+            } catch (Exception e) {
+                return "You failed to upload " + name + " => " + e.getMessage();
+            }
+        } else {
+            return "You failed to upload " + name + " because the file was empty.";
+        }
+    }
+
+}
--- vue/src/components/PetSubmissionForm.vue
+++ vue/src/components/PetSubmissionForm.vue
@@ -1,134 +1,180 @@
 <template>
-    <div class="main">
-        <div class="submission-form">
-            <form v-on:submit.prevent="submitForm">
-                <h2>Create New Pet</h2><br>
-                <div class="gender-radio">
-                    <label for="gender" id="gender" name="gender">Gender: </label>
-                    <input type="radio" id="gender-m" name="gender" value="M" v-model="newAnimal.gender">
-                    <label for="gender-m"> M</label>
-                    <input type="radio" id="gender-f" name="gender" value="F" v-model="newAnimal.gender">
-                    <label for="gender-f"> F</label>
-                </div>
-                <label for="petName">Pet Name: </label>
-                <input type="text" id="petName" name="petName" v-model="newAnimal.name">
-                <label for="age">Pet's age in months: </label>
-                <input type="number" id="age" name="age" v-model="newAnimal.age">
-                <label for="type-of-animal">What type of animal? </label>
-                <input type="text" id="type-of-animal" name="type-of-animal" v-model="newAnimal.species">
-                <label for="text" id="description" name="description" >Brief Description: </label>
-                <input type="text" id="description" name="description" v-model="newAnimal.description">
-                <label for="image-url">Image Url: </label>
-                <input type="text" name="image-url" id="image-url" v-model="newAnimal.imageUrl">
-
-                <div class="buttons">
-                    <button class="btn btn-submit">SUBMIT</button>
-                    <button class="btn btn-cancel" type="cancel" v-on:click.prevent="cancelForm">CANCEL</button>
-                </div>
-                
-            </form>
+  <div class="main">
+    <div class="submission-form">
+      <form v-on:submit.prevent="submitForm">
+        <h2>Create New Pet</h2>
+        <br />
+        <div class="gender-radio">
+          <label for="gender" id="gender" name="gender">Gender: </label>
+          <input
+            type="radio"
+            id="gender-m"
+            name="gender"
+            value="M"
+            v-model="newAnimal.gender"
+          />
+          <label for="gender-m"> M</label>
+          <input
+            type="radio"
+            id="gender-f"
+            name="gender"
+            value="F"
+            v-model="newAnimal.gender"
+          />
+          <label for="gender-f"> F</label>
         </div>
-    </div>    
+        <label for="petName">Pet Name: </label>
+        <input
+          type="text"
+          id="petName"
+          name="petName"
+          v-model="newAnimal.name"
+        />
+        <label for="age">Pet's age in months: </label>
+        <input type="number" id="age" name="age" v-model="newAnimal.age" />
+        <label for="type-of-animal">What type of animal? </label>
+        <input
+          type="text"
+          id="type-of-animal"
+          name="type-of-animal"
+          v-model="newAnimal.species"
+        />
+        <label for="text" id="description" name="description"
+          >Brief Description:
+        </label>
+        <input
+          type="text"
+          id="description"
+          name="description"
+          v-model="newAnimal.description"
+        />
+        <label for="image-url">Upload Image: </label>
+        <input type="file" @change="onFileChanged" />
+        <button @click="onUpload">Upload!</button>
+
+        <div class="buttons">
+          <button class="btn btn-submit">SUBMIT</button>
+          <button
+            class="btn btn-cancel"
+            type="cancel"
+            v-on:click.prevent="cancelForm"
+          >
+            CANCEL
+          </button>
+        </div>
+      </form>
+    </div>
+  </div>
 </template>
 
 <script>
-import animalService from '../services/AnimalService.js'
+import animalService from "../services/AnimalService.js";
+import axios from 'axios';
 
 export default {
-    name: "pet-submission-form",
-    data() {
-        return {
-            newAnimal: {
-                name: "",
-                gender: "",
-                age: "",
-                species: "",
-                description: "",
-                imageUrl: ""
-
-            }
-        };
+  name: "pet-submission-form",
+  data() {
+    return {
+      newAnimal: {
+        name: "",
+        gender: "",
+        age: "",
+        species: "",
+        description: "",
+        imageUrl: "",
+      },
+      selectedFile: null,
+    };
+  },
+  methods: {
+    cancelForm() {
+      this.$router.push("/");
+      this.newAnimal = {
+        name: "",
+        gender: "",
+        age: "",
+        species: "",
+        description: "",
+        imageUrl: "",
+      };
     },
-    methods: {
-        cancelForm() {
-            this.$router.push('/')
+    submitForm() {
+      const animalToAdd = {
+        name: this.newAnimal.name,
+        gender: this.newAnimal.gender,
+        age: this.newAnimal.age,
+        species: this.newAnimal.species,
+        description: this.newAnimal.description,
+        imageUrl: this.newAnimal.imageUrl,
+      };
+      animalService
+        .postAnimal(animalToAdd)
+        .then((response) => {
+          if (response.status === 200) {
+            alert("Submission successful");
             this.newAnimal = {
-                name: "",
-                gender: "",
-                age: "",
-                species: "",
-                description: "",
-                imageUrl: ""
-            }
-        },
-        submitForm() {
-            const animalToAdd = {
-                name: this.newAnimal.name,
-                gender: this.newAnimal.gender,
-                age: this.newAnimal.age,
-                species: this.newAnimal.species,
-                description: this.newAnimal.description,
-                imageUrl: this.newAnimal.imageUrl
-            }
-            animalService.postAnimal(animalToAdd)
-            .then(response => {
-                if (response.status === 200) {
-                    alert("Submission successful")
-                    this.newAnimal = {
-                        name: "",
-                        gender: "",
-                        age: "",
-                        species: "",
-                        description: "",
-                        imageUrl: ""
-                    }
-                }
-            })
-            .catch(error => {
-                alert("Error: try again : " + error.message)
-            })
-
-        }
-
-    }
-}
+              name: "",
+              gender: "",
+              age: "",
+              species: "",
+              description: "",
+              imageUrl: "",
+            };
+          }
+        })
+        .catch((error) => {
+          alert("Error: try again : " + error.message);
+        });
+    },
+    onFileChanged(event) {
+    this.selectedFile = event.target.files[0];
+    },
+    onUpload() {
+      const formData = new FormData();
+      formData.append("myFile", this.selectedFile);
+      console.log(formData);
+      axios.post("https://api.imgbb.com/1/upload?key=cf122104a5dfbf471b70ae94aea0eacd", formData)
+      .then(res => {
+          console.log(res);
+      })
+    },
+  },
+};
 </script>
 
 <style scoped>
 .main {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    min-height: 50vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  min-height: 50vh;
 }
 h2 {
-    text-align: center;
-    font-size: 2em;
+  text-align: center;
+  font-size: 2em;
 }
 .submission-form form {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
-    font-size: 14pt;
-    text-align: left;
-    width: 500px;
-    padding: 40px;
-    border: 2px solid rgba(248, 203, 4, 0.753);
-    background-color: rgba(250, 201, 154, 0.479);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  font-family: "Trebuchet MS", "Lucida Sans Unicode", "Lucida Grande",
+    "Lucida Sans", Arial, sans-serif;
+  font-size: 14pt;
+  text-align: left;
+  width: 500px;
+  padding: 40px;
+  border: 2px solid rgba(248, 203, 4, 0.753);
+  background-color: rgba(250, 201, 154, 0.479);
 }
 .submission-form input {
-    margin-top: 5px;
-    margin-bottom: 5px;
-
+  margin-top: 5px;
+  margin-bottom: 5px;
 }
 .submission-form .gender-radio {
-    text-align: center;
+  text-align: center;
 }
 .buttons {
-    margin-top: 10px;
+  margin-top: 10px;
 }
-
-
 </style>
