fd62fd625583ccb0490ee48ee9442189c66c33ea
Henry <hencutJohnson@gmail.com>
2021-08-09 13:47:05 -0400

    Revert "Created a new exception, UserDoesNotExistException"
    This reverts commit a3b672bfcd333b99f19142d87ee6c0c0e5e36dc6.

--- java/src/main/java/com/techelevator/model/UserDoesNotExistException.java
+++
@@ -1,8 +0,0 @@
-package com.techelevator.model;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "User does not exist.")
-public class UserDoesNotExistException extends RuntimeException{
-}
