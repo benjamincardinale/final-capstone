707320d888cdbb2b6afc1855c990b8c8b6f360cc
Henry <hencutJohnson@gmail.com>
2021-08-09 14:46:37 -0400

    Added a PetWithImage class to Model with an Image field. Changed a method in pet controller to take that PetWIthImage and INSERT a Pet

--- java/src/main/java/com/techelevator/controller/PetController.java
+++ java/src/main/java/com/techelevator/controller/PetController.java
@@ -1,10 +1,17 @@
 package com.techelevator.controller;
 
+import com.techelevator.Application;
 import com.techelevator.dao.JdbcPetDao;
+import com.techelevator.model.Image;
+import com.techelevator.model.ImageDto;
 import com.techelevator.model.Pet;
-import org.springframework.http.HttpStatus;
+import com.techelevator.model.PetWithImage;
+import org.springframework.http.*;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -14,9 +21,11 @@ import java.util.List;
 @PreAuthorize("isAuthenticated()")
 public class PetController {
     private JdbcPetDao jdbcPetDao;
+    private RestTemplate restTemplate;
 
-    public PetController(JdbcPetDao jdbcPetDao) {
+    public PetController(JdbcPetDao jdbcPetDao, RestTemplate restTemplate) {
         this.jdbcPetDao = jdbcPetDao;
+        this.restTemplate = restTemplate;
     }
 
     @PreAuthorize("permitAll")
@@ -25,10 +34,32 @@ public class PetController {
         return this.jdbcPetDao.getAllPets();
     }
 
+    private String addImage(Image imageBytes) throws Exception {
+        //System.out.println(imageBytes.getImage());
+        String url = "https://api.imgbb.com/1/upload?key=cf122104a5dfbf471b70ae94aea0eacd";
+        MultiValueMap<String, String> bodyMap = new LinkedMultiValueMap<>();
+        bodyMap.add("image", imageBytes.getImage());
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+        HttpEntity<MultiValueMap<String, String>> requestEntity = new HttpEntity<>(bodyMap, headers);
+
+        ResponseEntity<ImageDto> response = restTemplate.exchange(url, HttpMethod.POST, requestEntity, ImageDto.class);
+
+
+
+        return response.getBody().getDataUrl();
+    }
+
     //@ResponseStatus(HttpStatus.CREATED)
-    @RequestMapping(path = "/pet/add", method = RequestMethod.POST)
-    public Pet addPet(@Valid @RequestBody Pet pet) {
-        return jdbcPetDao.addPet(pet);
+    @RequestMapping(path = "/pet/add", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Pet addPet(@Valid @RequestBody PetWithImage pet) {
+        String imageUrl = null;
+        try {
+            imageUrl = addImage(pet.getImage());
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return jdbcPetDao.addPet(pet.getPet(imageUrl));
     }
     
     @RequestMapping(path = "/pet/update/{petId}", method = RequestMethod.PUT) //wew, come back to this one.
