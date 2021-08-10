8d1691df9ec1765d03eb263a0b959a26809ee626
Henry <hencutJohnson@gmail.com>
2021-08-10 14:17:48 -0400

    removed all of the fields of ChangePasswordDTO except for newPassword

--- java/src/main/java/com/techelevator/model/ChangePasswordDTO.java
+++ java/src/main/java/com/techelevator/model/ChangePasswordDTO.java
@@ -5,29 +5,7 @@ import javax.validation.constraints.NotEmpty;
 public class ChangePasswordDTO {
 
     @NotEmpty
-    private String username;
-    @NotEmpty
-    private String oldPassword;
-    @NotEmpty
     private String newPassword;
-    @NotEmpty
-    private String confirmNewPassword;
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getOldPassword() {
-        return oldPassword;
-    }
-
-    public void setOldPassword(String oldPassword) {
-        this.oldPassword = oldPassword;
-    }
 
     public String getNewPassword() {
         return newPassword;
@@ -35,14 +13,6 @@ public class ChangePasswordDTO {
 
     public void setNewPassword(String newPassword) {
         this.newPassword = newPassword;
-    }
-
-    public String getConfirmNewPassword() {
-        return confirmNewPassword;
-    }
-
-    public void setConfirmNewPassword(String confirmNewPassword) {
-        this.confirmNewPassword = confirmNewPassword;
     }
 
 }
